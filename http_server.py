b60e61138d970bbc492af4c0b53ffefd83be52af
Ian Auld <imauld@gmail.com>
2014-06-11 17:22:13 -0700

    Added proper HTTP responses

--- http_server.py
+++ http_server.py
@@ -1,8 +1,12 @@
 import socket
 from HTTPExceptions import HTTP400Error, HTTP404Error, HTTP405Error
 
+
+RESPONSE = '\r\n'.join(['HTTP/1.1 {} {}', 'Content-Type: text/plain', '', '{}'])
+
+
 def request_parser(raw_request):
-    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
     raw_request = raw_request.split('\r\n')
     keys = ('method', 'URI', 'protocol')
     request = dict(zip(keys, raw_request[0].split()))
@@ -10,32 +14,37 @@ def request_parser(raw_request):
         if element.lower().startswith('host:'):
             request['host'] = element.split()[1]
             break
-    return request if len(request) == 4 else 
+    if len(request) == 4:
+        return request
+    else:
+        raise HTTP400Error('Bad Request')
     
-    if len(request) != 3:
-        raise IndexError
-    method, URI, protocol, host_key, host = request[:5]
-    if method != "GET":
-        raise AttributeError
-    if URI[0] != "/":
-        raise SyntaxError
-    if protocol.strip() != "HTTP/1.1":
-        raise NameError
-    if host_key not in ("Host:"):
-        raise ValueError
-    return URI
-
+def check_request_method(request):
+    if request['method'] != 'GET':
+        raise HTTP405Error('Method Not Allowed')
+    
+def check_request_URI(request):
+    if not request['URI'].startswith('/'):
+        raise HTTP400Error('Bad Request')
+    
+def check_request_protocol(request):
+    if request['protocol'] != "HTTP/1.1":
+        raise HTTP400Error('Bad Request')
+    
+def check_request_host(request):
+    if 'host' not in request:
+        raise HTTP400Error('Bad Request')
 
 def http_server():
     SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     SERVER_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     SERVER_SOCKET.bind(('127.0.0.1', 50000))
+    buffsize = 32
+    SERVER_SOCKET.listen(1)
     print('Waiting for message...')
     while True:
         final_output = ''
         done = False
-        buffsize = 32
-        SERVER_SOCKET.listen(1)
         conn, addr = SERVER_SOCKET.accept()
         while not done:
             msg_part = conn.recv(buffsize)
@@ -43,18 +52,18 @@ def http_server():
             if len(msg_part) < buffsize:
                 done = True
         try:
-            URI = request_parser(final_output)
-            response = "200 OK: Your URI is {}".format(URI)
-        except IndexError:
-            response = "400 Bad Request"
-        except AttributeError:
-            response = "405 Method Not Allowed"
-        except SyntaxError:
-            response = "404 Not Found"
-        except NameError:
-            response = "400 Bad Request"
-        except ValueError:
-            response = "404 Not Found"
+            request = request_parser(final_output)
+            check_request_method(request)
+            check_request_URI(request)
+            check_request_protocol(request)
+            check_request_host(request)
+            response = RESPONSE.format('200', 'OK', 'This a message')
+        except HTTP400Error as err:
+            response = RESPONSE.format(400, err.message, '')
+        except HTTP404Error as err:
+            response = RESPONSE.format(404, err.message, '')
+        except HTTP405Error as err:
+            response = RESPONSE.format(405, err.message, '')
         conn.sendall(response)
         conn.close()
        # if final_output:
